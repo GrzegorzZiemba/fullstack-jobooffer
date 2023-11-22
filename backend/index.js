0fb032fc9203f4d191034a7a229fdeebfc63deb6
Grzesiek <gzgrzegorzziemba@gmail.com>
2023-11-22 11:38:15 +0100

    changes index.js to deploy

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,12 @@ import userRoutes from "./routes/userRoutes.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 createMongoDb();
@@ -31,12 +35,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/jobs", jobRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
-
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
   );
 }
 
